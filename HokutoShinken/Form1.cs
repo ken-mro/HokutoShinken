66486d87f74c4ca184b8a3978a984fed2bc56a97
Ken Murao <murao.ken.36x@kyoto-u.jp>
2023-04-23 01:04:42 +0900

    Add project files.

---
+++ HokutoShinken/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HokutoShinken
+{
+    public partial class Form1 : Form
+    {
+        private NotifyIcon notifyIcon;
+        KeyboardHook keyboardHook = new KeyboardHook();
+        private Shout shout = new Shout();
+
+        public Form1()
+        {
+            this.ShowInTaskbar = false;
+            this.setComponents();
+            shout.InitialShout(); 
+        }
+
+        private void setComponents()
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.Hokuto;
+            notifyIcon.Visible = true;
+            notifyIcon.Text = "HokutoShinken";
+
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            ToolStripMenuItem closeMenu = new ToolStripMenuItem();
+            closeMenu.Text = "&Decline the inheritance";
+            closeMenu.Click += ToolStripMenuItem_Click;
+            contextMenuStrip.Items.Add(closeMenu);
+
+            ToolStripMenuItem shoutCatchPhrase = new ToolStripMenuItem();
+            shoutCatchPhrase.Text = "Listen to the catch phrase!";
+            shoutCatchPhrase.Click += InitialShout_Click;
+            contextMenuStrip.Items.Add(shoutCatchPhrase);
+
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+
+            keyboardHook.KeyDownEvent += KeyboardHook_KeyDownEvent;
+            //keyboardHook.KeyUpEvent += KeyboardHook_KeyUpEvent;
+            keyboardHook.Hock();
+
+            notifyIcon.BalloonTipTitle = "Now you are the legitimate successor of Hokuto Shinken!";
+            notifyIcon.BalloonTipText = "Decline the inheritance? Right click on the icon in the Task Tray.";
+            notifyIcon.ShowBalloonTip(5000);
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            keyboardHook.UnHook();
+            Application.Exit();
+        }
+
+        private void InitialShout_Click(object sender, EventArgs e)
+        {
+            shout.InitialShout();
+        }
+
+        private void KeyboardHook_KeyDownEvent(object sender, KeyEventArgs e)
+        {
+            var key = e.KeyCode;
+            shout.Shouting(key);
+        }
+
+        //private void KeyboardHook_KeyUpEvent(object sender, KeyEventArgs e)
+        //{
+
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ HokutoShinken/Shout.cs
@@ -0,0 +1,79 @@
+﻿using System.Media;
+using System.Windows.Forms;
+
+namespace HokutoShinken
+{
+    public class Shout
+    {
+        private SoundPlayer _start;
+        private SoundPlayer _middle;
+        private SoundPlayer _end;
+        private SoundPlayer _hikou;
+        private SoundPlayer _catchPhrase;
+        private SoundPlayer _blast;
+        private SoundPlayer _current;
+        private System.Timers.Timer _StopTimer;
+        private bool _isShouting = false;
+
+        public Shout()
+        {
+            _start = new SoundPlayer();
+            _start.Stream = Properties.Resources.ata;
+            _middle = new SoundPlayer();
+            _middle.Stream = Properties.Resources.tatatata;
+            _end = new SoundPlayer();
+            _end.Stream = Properties.Resources.acha;
+            _hikou = new SoundPlayer();
+            _hikou.Stream = Properties.Resources.hikou;
+            _catchPhrase = new SoundPlayer();
+            _catchPhrase.Stream = Properties.Resources.catchphrase;
+            _blast = new SoundPlayer();
+            _blast.Stream = Properties.Resources.blast;
+
+            _StopTimer = new System.Timers.Timer(275);
+            _StopTimer.Elapsed += OnTimedEvent;
+            _StopTimer.AutoReset = false;
+        }
+
+        public void InitialShout()
+        {
+            _catchPhrase.PlaySync();
+            _hikou.PlaySync();
+            _blast.PlaySync();
+        }
+        public void Shouting(Keys key)
+        {
+            if (key == Keys.Enter)
+            {
+                _end.PlaySync();
+                return;
+            }
+
+            if (key == Keys.Space || ((int)Keys.A <= (int)key && (int)key <= (int)Keys.Z))
+            {
+                _StopTimer.Enabled = false;
+                if (_isShouting)
+                {
+                    if (_current != _middle)
+                    {
+                        _middle.PlayLooping();
+                        _current = _middle;
+                    }
+                }
+                else
+                {
+                    _isShouting = true;
+                    _start.Play();
+                    _current = _start;
+                }
+                _StopTimer.Enabled = true;
+            }
+        }
+
+        private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _current.Stop();
+            _isShouting = false;
+        }
+    }
+}
